9f2ee7cf4b9eeed963e0933d301106f62e1d3477
blk-im <83579043+blk-im@users.noreply.github.com>
2025-08-10 19:30:51 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -129,7 +129,7 @@ const fetchCryptoNews = async () => {
     }
 
     const coindeskData = await coindeskResponse.json();
-    const formattedNews = coindeskData.map(item => ({
+    const formattedNews = coindeskData.data.items.map(item => ({
       id: item.ID.toString(),
       title: item.TITLE,
       summary: item.BODY,
